3c0fc2a003b7cebe3af08d1dfcab0719f0ecbea5
Sean San <seanksan@gmail.com>
2025-11-10 00:00:10 -0800

    Set up the event listeners for all buttons

--- index.js
+++ index.js
@@ -1,18 +1,99 @@
+let firstOperand = "";
+let secondOperand = "";
+let operation = "";
+
 const q_clear = document.querySelector("#clear");
+q_clear.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
+
 const q_delete = document.querySelector("#delete");
+q_delete.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_seven = document.querySelector("#seven");
+q_seven.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_eight = document.querySelector("#eight");
+q_eight.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_nine = document.querySelector("#nine");
+q_nine.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_four = document.querySelector("#four");
+q_four.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_five = document.querySelector("#five");
+q_five.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_six = document.querySelector("#six");
+q_six.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_one = document.querySelector("#one");
+q_one.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_two = document.querySelector("#two");
+q_two.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_three = document.querySelector("#three");
+q_three.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_zero = document.querySelector("#zero");
+q_zero.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_dot = document.querySelector("#dot");
+q_dot.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_equal = document.querySelector("#equal");
+q_equal.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_plus = document.querySelector("#plus");
+q_plus.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_minus = document.querySelector("#minus");
+q_minus.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_asteriks = document.querySelector("#asteriks");
+q_asteriks.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
 const q_backward_slash = document.querySelector("#backward-slash");
+q_backward_slash.addEventListener("click", function() {
+    console.log('Button was clicked!');
+});
+
+firstOperand = firstOperand + "3";
+secondOperand = secondOperand + "4";
+operation = "*";
+console.log(parseInt(firstOperand) + parseInt(secondOperand));
